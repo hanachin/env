06da7b04e3922154a000fc1034c75fa410b1c95e
Seiei Miyagi <hanachin@gmail.com>
2018-09-23 16:45:56 +0900

    Fix rust-racer cookbook

--- cookbooks/rust-racer.rb
+++ cookbooks/rust-racer.rb
@@ -5,7 +5,7 @@ end
 
 execute "~/.cargo/bin/rustup component add rust-src" do
   user "sei"
-  not_if '[ -n "$(~/.cargo/bin/rustup component list | grep rust-src)" | grep installed ]'
+  not_if '[ -n "$(~/.cargo/bin/rustup component list | grep rust-src | grep installed )" ]'
 end
 
 execute "~/.cargo/bin/cargo +nightly install racer" do

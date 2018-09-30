bc2bb1dafa4b335a0e44c59a48f7a09b7e81fa5c
Seiei Miyagi <hanachin@gmail.com>
2018-10-01 01:47:57 +0900

    Update cookbooks, use apt install instaed of dpkg --install

--- cookbooks/dropbox.rb
+++ cookbooks/dropbox.rb
@@ -1,5 +1,3 @@
-execute "wget -O - 'https://www.dropbox.com/download?plat=lnx.x86_64' | tar xzf -" do
-  cwd "/home/sei"
-  user "sei"
-  not_if "[ -d ~/.dropbox-dist ]"
+execute "wget -qO /tmp/dropbox.deb https://www.dropbox.com/download?dl=packages/ubuntu/dropbox_2015.10.28_amd64.deb && apt install -y /tmp/dropbox.deb" do
+  not_if "[ -x /usr/bin/dropbox ]"
 end
--- cookbooks/zoomus.rb
+++ cookbooks/zoomus.rb
@@ -1,4 +1,4 @@
 package "libxcb-xtest0"
-execute "wget -qO /tmp/zoom_amd64.deb https://zoom.us/client/latest/zoom_amd64.deb && dpkg --install /tmp/zoom_amd64.deb" do
+execute "wget -qO /tmp/zoom_amd64.deb https://zoom.us/client/latest/zoom_amd64.deb && apt install -y /tmp/zoom_amd64.deb" do
   not_if "[ -x /usr/bin/zoom ]"
 end

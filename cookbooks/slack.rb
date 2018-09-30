f34bdf6e0b2cb72a67979658b0963b1c92b12199
Seiei Miyagi <hanachin@gmail.com>
2018-10-01 01:36:59 +0900

    Update zoom.us and slack installation guard

--- cookbooks/slack.rb
+++ cookbooks/slack.rb
@@ -1,3 +1,5 @@
 package "gconf2"
 package "libappindicator1"
-execute "wget -qO /tmp/slack-desktop.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.1-amd64.deb && dpkg --install /tmp/slack-desktop.deb"
+execute "wget -qO /tmp/slack-desktop.deb https://downloads.slack-edge.com/linux_releases/slack-desktop-3.3.1-amd64.deb && dpkg --install /tmp/slack-desktop.deb" do
+  not_if "[ -x /usr/bin/slack ]"
+end
--- cookbooks/zoomus.rb
+++ cookbooks/zoomus.rb
@@ -1,2 +1,4 @@
 package "libxcb-xtest0"
-execute "wget -qO /tmp/zoom_amd64.deb https://zoom.us/client/latest/zoom_amd64.deb && dpkg --install /tmp/zoom_amd64.deb"
+execute "wget -qO /tmp/zoom_amd64.deb https://zoom.us/client/latest/zoom_amd64.deb && dpkg --install /tmp/zoom_amd64.deb" do
+  not_if "[ -x /usr/bin/zoom ]"
+end
